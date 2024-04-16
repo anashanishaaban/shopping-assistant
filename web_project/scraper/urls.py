77e5a8c85f98cf65e808d1cfb908f2e0a9b2d130
anashanishaaban <anooshani@yahoo.com>
2024-04-16 13:34:38 -0400

    fixing up

--- web_project/scraper/urls.py
+++ web_project/scraper/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('landing/', views.search_results, name='search_results'),
-
-    path('profile/', views.profile, name='profile'),
     path('wishlist/', views.wishlist, name='wishlist'),
     path('analyze-image/', views.analyze_image, name='analyze_image'),
 
