02d1f08ed1ae9b23cc35c7cd3b02f9d97b3b240b
Anas Shaaban <112586546+turtleslayz@users.noreply.github.com>
2024-05-06 08:23:49 +0300

    finally fixed chatbot

--- web_project/scraper/urls.py
+++ web_project/scraper/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='landing'),
+    path('api/', views.api_view, name='api_view'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('landing/', views.search_results, name='search_results'),
     path('wishlist/', views.wishlist, name='wishlist'),
-    path('api', views.api_view, name='api_view')
 ]
